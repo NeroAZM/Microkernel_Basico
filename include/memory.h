39ae5991c8ad1130f79fae8d166b2e1a595c43fd
Nilson Hoffmann Neto <nilson.hoffmann@trust.group>
2026-06-08 20:18:20 -0300

    feat: removido comentarios redundantes

--- include/memory.h
+++ include/memory.h
@@ -1,16 +1,11 @@
 #pragma once
 #include <stdint.h>
 
-/* Inicializa o heap */
 void memory_init(void);
 
-/* Alocação simples */
 void *kmalloc(uint64_t size);
-
-/* Liberação com coalescência e reutilização */
 void kfree(void *ptr);
 
-/* Informações */
 uint64_t memory_used(void);
 uint64_t memory_free(void);
 uint64_t memory_total(void);
