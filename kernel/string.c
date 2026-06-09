8b114fc118df1f0d86856257456c279198708448
Lucas Coelho <161223249+NeroAZM@users.noreply.github.com>
2026-06-09 00:27:25 -0300

    Implement memcpy function for memory copying

--- kernel/string.c
+++ kernel/string.c
@@ -1,5 +1,14 @@
 #include <stdint.h>
 
+void *memcpy(void *dest, const void *src, uint64_t n)
+{
+    uint8_t *d = dest;
+    const uint8_t *s = src;
+    while (n--)
+        *d++ = *s++;
+    return dest;
+}
+
 void *memset(void *s, int c, uint64_t n)
 {
     uint8_t *p = s;
